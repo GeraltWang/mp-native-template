d3f779681b9123293da5c041277685dd6282a9da
GeraltWang <WGERALT@OUTLOOK.COM>
2023-02-10 23:45:55 +0800

    UP echarts演示页面

---
+++ package-charts/components/my-ec-canvas/index.js
@@ -0,0 +1,59 @@
+// package-charts/components/my-ec-canvas/index.js
+import * as echarts from '../../libs/echarts.min'
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    chartLineId: { type: String },
+    canvasId: { type: String },
+    options: { type: Object },
+    height: {
+      type: String,
+      value: '100%'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    ec: {
+      lazyLoad: true // 设置echarts延时加载
+    }
+  },
+  lifetimes: {
+    ready () {
+      this[this.data.chartLineId] = this.selectComponent('#' + this.data.chartLineId) // 通过`id`获取echarts组件
+      this.initChart()
+    },
+    detached (e) {
+      this[this.data.chartLineId] = null
+      this[this.data.canvasId] = null
+    }
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    initChart () {
+      this[this.data.chartLineId].init((canvas, width, height, dpr) => {
+        const chart = echarts.init(canvas, null, {
+          width,
+          height,
+          devicePixelRatio: dpr // new
+        })
+        canvas.setChart(chart)
+        chart.setOption(this.getOption())
+        return chart
+      })
+    },
+    getOption () {
+      const option = this.data.options
+      console.log(option)
+      return option
+    }
+  }
+})
