67ec42e4b63906d4bd8c0f422363135cf57f3b26
王昶 <wgeralt@outlook.com>
2023-02-20 16:40:25 +0800

    UP 增加必要的注释

--- utils/wx/interaction.js
+++ utils/wx/interaction.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 王昶 wgeralt@outlook.com
+ * @Date: 2023-02-12 22:08:44
+ * @LastEditors: 王昶 wgeralt@outlook.com
+ * @LastEditTime: 2023-02-20 13:55:30
+ * @FilePath: /mp-native-template/utils/wx/interaction.js
+ * @Description:
+ */
 /** getWxOpenId
  * @param {object} data toast配置项
  * @param {function} success 成功后执行的函数
@@ -28,6 +36,13 @@ export const showToast = (
     })
 }
 
+/**
+ * @description: showLoading
+ * @param {object} data
+ * @param {string} data.title loading文字
+ * @param {boolean} data.mask 是否展示遮罩
+ * @return {void}
+ */
 export const showLoading = ({ title = '加载中', mask = true } = {}) => {
   wx.showLoading({
     title,
--- utils/wx/location.js
+++ utils/wx/location.js
@@ -1,5 +1,14 @@
+/*
+ * @Author: 王昶 wgeralt@outlook.com
+ * @Date: 2023-02-12 22:08:44
+ * @LastEditors: 王昶 wgeralt@outlook.com
+ * @LastEditTime: 2023-02-20 13:56:20
+ * @FilePath: /mp-native-template/utils/wx/location.js
+ * @Description:
+ */
+
 /** getLocation
- * @param {object} options 获取位置需要传入的数据
+ * @param {object} options 获取位置需要传入的数据参考wx.getLocation()
  */
 export const getLocation = (
   options = {
