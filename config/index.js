e8f8ae0f7c08bf370811ae4fea4a2af8bf4b1370
王昶 <wgeralt@outlook.com>
2023-03-29 11:28:06 +0800

    UP 完善config配置项

--- config/index.js
+++ config/index.js
@@ -2,20 +2,23 @@
  * @Author: 王昶 wgeralt@outlook.com
  * @Date: 2023-02-12 22:08:44
  * @LastEditors: 王昶 wgeralt@outlook.com
- * @LastEditTime: 2023-03-08 17:49:23
+ * @LastEditTime: 2023-03-29 11:27:52
  * @FilePath: /mp-native-template/config/index.js
  * @Description: 小程序配置文件
  */
+import szCoinBase from './szCoinBase'
+
 export default {
   // 小程序名称
   mpName: 'MP-Template',
   // 小程序版本
   mpVersion: '1.0.0',
   // 接口基地址
-  // apiBaseUrl: 'http://svc-test.jshr.cn',
   apiBaseUrl: 'https://mis-test.51haohuo.com/coin-service/app',
   // 对象存储基地址
   ossBaseUrl: '',
+  // 默认 loading 文字
+  baseLoadingText: '加载中...',
   // token 名称
   tokenName: 'token',
   // 默认请求头
@@ -23,5 +26,8 @@ export default {
   // 为本地存储添加统一的前缀
   storagePrefix: '',
   // 本地存储过期时间 单位（天）
-  storageExpireTime: 0
+  storageExpireTime: 0,
+  // web-view地址
+  webviewUrl: {},
+  szCoinBase
 }
---
+++ config/szCoinBase.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: 王昶 wgeralt@outlook.com
+ * @Date: 2023-03-29 10:14:09
+ * @LastEditors: 王昶 wgeralt@outlook.com
+ * @LastEditTime: 2023-03-29 10:14:39
+ * @FilePath: /mp-native-template/config/szCoinBase.js
+ * @Description:
+ */
+export default {
+  // 接口基地址
+  apiBaseUrl: 'https://mis.51haohuo.com/coin-service',
+  // 对象存储基地址
+  ossBaseUrl: '',
+  // token 名称
+  tokenName: 'Authorization',
+  // token prefix
+  tokenPrefix: '',
+  // 默认请求头
+  header: {}
+}
