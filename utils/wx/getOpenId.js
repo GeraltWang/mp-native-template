7bb76052754ef746ec6852c3dd9db2ede8f5cc3c
王昶 <wgeralt@outlook.com>
2023-04-14 17:30:01 +0800

    UP getOpenId方法完善

--- utils/wx/getOpenId.js
+++ utils/wx/getOpenId.js
@@ -2,35 +2,45 @@
  * @Author: 王昶 wgeralt@outlook.com
  * @Date: 2023-02-12 22:08:44
  * @LastEditors: 王昶 wgeralt@outlook.com
- * @LastEditTime: 2023-03-08 17:57:06
+ * @LastEditTime: 2023-04-14 16:11:07
  * @FilePath: /mp-native-template/utils/wx/getOpenId.js
  * @Description:
  */
 import { getOpenId } from '../../api/sz-coin-base/model/test'
+import { CODE_ENUM } from '../../api/sz-coin-base/enum/index'
 import { user } from '../../store/index'
 
+// 已重试次数
 let retryCount = 0
 
 /** getWxOpenId
  * @description: getWxOpenId,调用wx.login成功后，调用后台获取openid方法
- * @param {object} globalData app.js中的globalData
  * @param {string} maxRetry 重试次数
  */
-const getWxOpenId = async (globalData, maxRetry = 2) => {
-  try {
-    const wxRes = await wx.p.login()
-    globalData.wxcode = wxRes.code
-    const apiRes = await getOpenId({
-      js_code: wxRes.code
-    })
-    user.updateOpenid(apiRes.data.openid || null, true)
-  } catch (error) {
+const getWxOpenId = async (maxRetry = 2) => {
+  // 调用wx.login获取code
+  const wxRes = await wx.p.login()
+  // 调用后台获取openid
+  const [error, apiRes] = await getOpenId({
+    js_code: wxRes.code
+  })
+  // 获取opedid失败
+  if (error) {
+    // 401 403 不重试
+    if (error.code === CODE_ENUM.FORBIDDEN || error.code === CODE_ENUM.UNAUTHORIZED) {
+      console.log(`openid获取失败, 失败次数:${retryCount}`, `失败原因:${error.msg}`)
+      return [error, null]
+    }
+    // 重试次数超过最大值 停止重试
     if (retryCount >= maxRetry) {
-      throw new Error('获取openid超过最大次数')
+      console.log(`openid获取失败, 失败次数:${retryCount}`, `失败原因:${error.msg}`)
+      return [error, null]
     }
     retryCount++
-    console.log(`openid获取失败, 失败次数:${retryCount}`, `失败原因:${error.message}`)
-    await getWxOpenId(globalData, maxRetry = 2)
+    await getWxOpenId(maxRetry = 2)
+  // 获取openid成功
+  } else {
+    user.updateOpenid(apiRes.data.openid || null, true)
   }
 }
 
