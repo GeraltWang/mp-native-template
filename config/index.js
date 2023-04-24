a01291b452868c56a845dee44a364e47933c343f
王昶 <wgeralt@outlook.com>
2023-04-24 13:14:19 +0800

    up 完善config配置

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
  * @Author: 王昶 wgeralt@outlook.com
  * @Date: 2023-02-12 22:08:44
  * @LastEditors: 王昶 wgeralt@outlook.com
- * @LastEditTime: 2023-03-29 11:27:52
+ * @LastEditTime: 2023-04-24 13:11:35
  * @FilePath: /mp-native-template/config/index.js
  * @Description: 小程序配置文件
  */
@@ -13,6 +13,8 @@ export default {
   mpName: 'MP-Template',
   // 小程序版本
   mpVersion: '1.0.0',
+  // 小程序主题色
+  mpThemeColor: '#336CFF',
   // 接口基地址
   apiBaseUrl: 'https://mis-test.51haohuo.com/coin-service/app',
   // 对象存储基地址
