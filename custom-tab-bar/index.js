fdc34a033a8edf4583561edb6eebb4e40b3c688d
王昶 <wgeralt@outlook.com>
2023-04-26 16:41:45 +0800

    fix 修复tabbaricon闪烁的问题

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 王昶 wgeralt@outlook.com
+ * @Date: 2023-02-12 22:08:44
+ * @LastEditors: 王昶 wgeralt@outlook.com
+ * @LastEditTime: 2023-04-26 16:14:14
+ * @FilePath: /mp-native-template/custom-tab-bar/index.js
+ * @Description:
+ */
 import TabMenu from './data'
 Component({
   data: {
@@ -6,7 +14,7 @@ Component({
   },
   methods: {
     onChange (event) {
-      this.setData({ active: event.detail.value })
+      // this.setData({ active: event.detail.value })
       const index = event.detail.value
       wx.switchTab({
         url: this.data.list[index].url.startsWith('/')
