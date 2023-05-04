4fc91e17f3d34f4d32907e57849505be0d201f43
王昶 <wgeralt@outlook.com>
2023-05-04 17:11:30 +0800

    fix 修复getPermission方法

--- utils/wx/getPermission.js
+++ utils/wx/getPermission.js
@@ -2,7 +2,7 @@
  * @Author: 王昶 wgeralt@outlook.com
  * @Date: 2023-02-12 22:08:44
  * @LastEditors: 王昶 wgeralt@outlook.com
- * @LastEditTime: 2023-04-13 14:33:47
+ * @LastEditTime: 2023-05-04 17:07:52
  * @FilePath: /mp-native-template/utils/wx/getPermission.js
  * @Description:
  */
@@ -12,74 +12,41 @@
  * @param {string} data.name 权限名称(可自定义)
  */
 
-const getPermission = ({ code, name }) => {
-  return wx.p.getSetting().then(res => {
-    // 用户已经拒绝过授权
+export const getPermission = async ({ code, name }) => {
+  try {
+    const res = await wx.p.getSetting()
     if (res.authSetting[code] === false) {
-      return wx.p.showModal({
-        title: `获取${name}失败`,
-        content: `获取${name}失败，请在【右上角】-小程序【设置】项中，将【${name}】开启。`,
-        confirmText: '去设置',
-        // confirmColor: '#FA550F',
-        cancelColor: '取消'
-      }).then(res => {
-        // 用户点击了去设置
-        if (res.confirm) {
-          return wx.p.openSetting().then(settingRes => {
-            if (settingRes.authSetting[code] === true) {
-              return Promise.resolve()
-            } else {
-              return Promise.reject(new Error('用户未打开权限'))
-            }
-          }).catch((error) => {
-            return Promise.reject(error)
-          })
-        // 用户点击了取消
-        } else {
-          return Promise.reject(new Error(`获取${name}被用户取消`))
-        }
-      }).catch((error) => {
-        return Promise.reject(error)
-      })
-    // 用户未拒绝过授权
+      return showModal(code, name)
     } else if (res.authSetting[code] === undefined) {
-      return wx.p.authorize({
-        scope: code
-      }).catch(() => {
-        return wx.p.showModal({
-          title: `获取${name}失败`,
-          content: `获取${name}失败，请在【右上角】-小程序【设置】项中，将【${name}】开启。`,
-          confirmText: '去设置',
-          // confirmColor: '#FA550F',
-          cancelColor: '取消'
-        }).then(res => {
-          // 用户点击了去设置
-          if (res.confirm) {
-            return wx.p.openSetting().then(settingRes => {
-              if (settingRes.authSetting[code] === true) {
-                return Promise.resolve()
-              } else {
-                console.warn('用户未打开权限', name, code)
-                return Promise.reject(new Error('用户未打开权限'))
-              }
-            }).catch((error) => {
-              return Promise.reject(error)
-            })
-          // 用户点击了取消
-          } else {
-            return Promise.reject(new Error(`获取${name}被用户取消`))
-          }
-        }).catch((error) => {
-          return Promise.reject(error)
-        })
-      })
-    // 用户已经授权
+      await wx.p.authorize({ scope: code })
+      return Promise.resolve()
     } else {
       return Promise.resolve()
     }
-  }).catch(error => {
+  } catch (error) {
     return Promise.reject(error)
-  })
+  }
 }
 
-export default getPermission
+const showModal = async (code, name) => {
+  try {
+    const res = await wx.p.showModal({
+      title: `获取${name}失败`,
+      content: `获取${name}失败，请在【右上角】-小程序【设置】项中，将【${name}】开启。`,
+      confirmText: '去设置'
+    })
+    if (res.confirm) {
+      const settingRes = await wx.p.openSetting()
+      if (settingRes.authSetting[code] === true) {
+        return Promise.resolve()
+      } else {
+        console.warn('用户未打开权限', name, code)
+        return Promise.reject(new Error('用户未打开权限'))
+      }
+    } else {
+      return Promise.reject(new Error(`获取${name}被用户取消`))
+    }
+  } catch (error) {
+    return Promise.reject(error)
+  }
+}
