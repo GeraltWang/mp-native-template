c5ac2655560580e684b5a7ad693c61c41646e6b6
GeraltWang <58298096+GeraltWang@users.noreply.github.com>
2023-07-17 23:43:07 +0800

    chore(dependencies): add husky lint-staged commitlint

---
+++ commitlint.config.cjs
@@ -0,0 +1,11 @@
+/*
+ * @Author: GeraltWang
+ * @Date: 2023-07-17 21:38:07
+ * @LastEditors: GeraltWang
+ * @LastEditTime: 2023-07-17 21:38:17
+ * @FilePath: \mp-native-template\commitlint.config.cjs
+ * @Description:
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+}
